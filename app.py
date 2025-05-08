e674ee9912eeaf0c440468beda186afbf71c3ea6
serriouiferdaouss-digital <ferdaoussserrioui@gmail.com>
2025-05-08 21:31:28 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+import os
+import gdown
+
+# 🔁 Télécharger le modèle depuis Google Drive
+MODEL_URL = "https://drive.google.com/uc?id=1t1pGYE7PZXl6o0--p8xenRlA_uY-GG80"
+MODEL_PATH = "pneumonia_detector_model.h5"
+
+if not os.path.exists(MODEL_PATH):
+    with st.spinner("🔁 Downloading model..."):
+        gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+
+# ✅ Charger le modèle
+model = load_model(MODEL_PATH)
+
+st.title("🩺 Pneumonia Detection from Chest X-ray")
+
+uploaded_file = st.file_uploader("Upload a Chest X-ray image", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image', use_column_width=True)
+
+    img = img.resize((224, 224))
+    img_array = image.img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)[0][0]
+
+    if prediction > 0.5:
+        st.error(f"🟥 Pneumonia detected with confidence {prediction:.2f}")
+    else:
+        st.success(f"🟩 Normal with confidence {1 - prediction:.2f}")
